1d6c27505029fb8db5bcfcc0937c8f2d55d374c5
Kwilz <aaleksej809@gmail.com>
2023-09-08 14:08:32 +0400

    UP

---
+++ !~discordClearCache.cmd
@@ -0,0 +1,8 @@
+@echo OFF
+rd "%appdata%/discord/Cache" /s /q
+rd "%appdata%/discord/Code Cache" /s /q
+rd "%appdata%/discord/Crashpad" /s /q
+rd "%appdata%/discord/DawnCache" /s /q
+rd "%appdata%/discord/GPUCache" /s /q
+rd "%appdata%/discord/DawnCache" /s /q
+rd "%appdata%/discord/Service Worker/CacheStorage" /s /q
